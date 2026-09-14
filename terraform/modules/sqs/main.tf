4c297a6dec737ebd85ffce42a4f0eb61cf58c314
Pedro Costa <pedrohenriqueortizcosta@gmail.com>
2026-09-14 20:57:41 +0930

    Preserve SQS long-polling default (receive_wait_time_seconds=20)

--- terraform/modules/sqs/main.tf
+++ terraform/modules/sqs/main.tf
@@ -7,6 +7,7 @@ resource "aws_sqs_queue" "this" {
   name                       = var.queue_name
   visibility_timeout_seconds = 60
   message_retention_seconds  = 345600 # 4 days
+  receive_wait_time_seconds  = 20     # long polling, matches analytics-service's consumer
 
   redrive_policy = jsonencode({
     deadLetterTargetArn = aws_sqs_queue.dlq.arn
